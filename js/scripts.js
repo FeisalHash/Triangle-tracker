0a13cb1488e41731fa5684ee1dc185426cb769d8
Feisal Hasham <feisalhasham98@gmail.com>
2018-08-10 11:51:36 +0300

    variables defined

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,4 @@
-unction getTypeOfTriangle(){
+function getTypeOfTriangle(){
+  var side1 = parseFloat(prompt("Please input side 1"));
+  var side2 = parseFloat(prompt("Please input side 2"));
+  var side3 = parseFloat(prompt("Please input side 3"));
