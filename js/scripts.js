f1ef71b2ae1d3a8ab04a40d929b8ded9c01df808
Feisal Hasham <feisalhasham98@gmail.com>
2018-08-10 12:01:03 +0300

    prompts created

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,5 @@ function getTypeOfTriangle(){
   var side1 = parseFloat(prompt("Please input side 1"));
   var side2 = parseFloat(prompt("Please input side 2"));
   var side3 = parseFloat(prompt("Please input side 3"));
+}
+getTypeOfTriangle();
